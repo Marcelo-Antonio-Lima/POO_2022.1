18d6b0960a3cacf3c6c26f12cf356353eaef268a
Marcelo-Antonio-Lima <84820723+Marcelo-Antonio-Lima@users.noreply.github.com>
2022-04-07 21:06:54 -0300

    Update 3_Q1.java

--- Poo/Atividade Operadores e Sintaxe/3_Q1.java
+++ Poo/Atividade Operadores e Sintaxe/3_Q1.java
@@ -1,22 +1,26 @@
-/*
-class Main {
+class LaçoCondicional { //Laço é executado enquanto a condição for verdadeira
   public static void main(String[] args) {
-    int varx=13;
-    while(varx != 1){
-      if(varx % 2 == 0){
-        varx = varx/2;
-        if (varx == 1){
+    
+    int varx=13; //Variável a ser testada
+    
+    while(varx != 1){ //O laço ocorre enquanto varx for diferente de 1
+      
+      if(varx % 2 == 0){ //Testa se varx é par
+        varx = varx/2; //Caso seja varx é dividido por 2
+        
+        if (varx == 1){ // Caso varx seja 1 será imprimido apenas varx 
           System.out.print(varx);
         }
+        
         else{
-          System.out.print(varx+" -->");
+          System.out.print(varx+" -->");// Caso varx seja diferente de 1 será imprimido "varx -->"
         }
+        
       }  
-      else{          
-        varx = 3 * varx + 1;
+      else{ // Caso varx seja impar varx é multiplicado por 3 e somado 1, então é imprimido     
+        varx = 3 * varx + 1; 
         System.out.print(varx+" -->");
       }
     }    
   }
 }
-*/
